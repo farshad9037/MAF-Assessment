31b69325b49ca0b3a4f52a515f246307183bcded
Farshad <farshadcp@gmail.com>
2018-07-02 01:21:31 +0530

    Validation added.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
-    $("form").submit(function(){
-        alert("Submitted");
+    $("#validate").bind('click', function () {
+        var username = $('#username'),
+            password = $('#password');
+ 
+        if (validateEmail( username.val() ) ) {
+            username.removeClass('has-error');
+            $('#userNameLabel').removeClass('pull-right').css({
+                color: '#333',
+            }).text('Username');
+        } else {
+            username.addClass('has-error');
+            $('#userNameLabel').addClass('pull-right').css({
+                color: 'red',
+            }).text('Required field');
+            return;
+        }
+
+        if ( password.val() ) {
+            $('#passRequired').hide();
+            password.removeClass('has-error');
+        } else {
+            $('#passRequired').show();
+            password.addClass('has-error');
+            return;
+        }
+
+        authenticate(username.val(), password.val());
     });
+
+    function authenticate(username, password) {
+        $.post('sampleApi', {
+            username: username,
+            password : password
+        }, function(returnedData){
+            console.log(returnedData);
+        }).fail(function(){
+            alert('Mock login authentication triggered');
+        });
+    }
+
+    function validateEmail(email) {
+        var emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegEx.test(String(email).toLowerCase());
+    }
 });
